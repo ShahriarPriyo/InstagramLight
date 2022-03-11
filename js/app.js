f435928abff2093d8f5fc3ee5f8dfef00b208160
ShahriarPriyo <shahriarpriyo98@gmail.com>
2022-03-11 12:28:07 +0600

    final bug fixed

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-  likedPostsId.push(id); //3rd bug fixed 
+  likedPostsId.push(id); //3rd bug fixed:line 19
   showPosts(posts);
 };
 
@@ -108,7 +108,7 @@ const createPost = (post) => {
                 <div class="post__infos">
                   <div class="post__likes">
                     <a href="#" class="post__likes-avatar">
-                      <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="User Picture" />
+                      <img src="${userImage}" alt="User Picture" />
                     </a>
 
                     <span>Liked by
@@ -146,7 +146,9 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById('liked').innerHTML = ''; //final bug fixed:line 149 -> liked posts
   const likedPosts = getLikedPosts();
+
   likedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("liked").appendChild(div);
